779cceba9dc16b0825040b419cc6551537d96463
lderecho12 <lderecho12@gmail.com>
2017-11-06 21:35:47 +0800

    3rd| Added hotelCashier login background, added loginstyle css and initialized login.php file.

---
+++ views/account/login.php
@@ -0,0 +1,43 @@
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>LogIn || Fleur Suites</title>
+        <script src="../js/jquery.js"></script>
+        <script src="../js/navscroll.js"></script>
+        <script src="../js/homeSlideshow.js"></script>
+        <link rel="stylesheet" type="text/css" href="../styles/loginstyle.css">
+        <link rel="stylesheet" type="text/css" href="../styles/navbarstyle.css">
+        <link rel="stylesheet" type="text/css" href="../styles/slideshowstyle.css">
+    </head>
+
+    <body>
+    
+        <header>
+            <div class="navbar">       
+                <nav class="mynavbar">
+                    <ul>
+                        <li class="myreservations"><a href="myreservations.html">My Reservations</a></li>
+                        <li class="login"><a href="login.php">LogIn</a></li>
+                    </ul>
+                </nav>
+                <nav class="navbar">
+                    <ul>
+                        <li class="home"><a href="index.html">Home</a></li>
+                        <li class="accomodations"><a href="accomodations.html">Accomodations</a></li>
+                        <li class="contactus"><a href="contactus.html">Contact Us</a></li>
+                        <li class="aboutus"><a href="aboutus.html">About Us</a></li>
+                    </ul>
+                </nav>
+                <a class="navbarlogo" href="aboutme.html">
+                    <img alt="logo" src="../images/logos/LanceLogoWhite.png"/>
+                </a> 
+            </div>  
+        </header>   
+
+        <div class="backgroundlogin">
+            <img src="../images/hotelCashier.png" style="width:100%">
+        </div>
+
+    </body>
+
+</html>
